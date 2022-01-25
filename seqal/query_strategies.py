4d955d8e5fb0c7db20c4985345b4a839d924979d
BrambleXu <liangxu006@gmail.com>
2022-01-25 16:39:52 +0900

    refactor: update similarity cluster sampling

--- seqal/query_strategies.py
+++ seqal/query_strategies.py
@@ -1,10 +1,63 @@
 import random
+from pickletools import float8
 from typing import List
 
 import numpy as np
+import torch
 from flair.data import Sentence
+from flair.embeddings import StackedEmbeddings
 from sklearn.cluster import KMeans
-from torch import nn, stack
+
+
+def sim_matrix(a: torch.tensor, b: torch.tensor, eps: float8 = 1e-8) -> torch.tensor:
+    """Calculate similarity bewteen matrix
+
+    Args:
+        a (torch.tensor): Matrix of embedding. shape=(entity_count, embedding_dim)
+        b (torch.tensor): Matrix of embedding. shape=(entity_count, embedding_dim)
+        eps (float8, optional): Eps for numerical stability. Defaults to 1e-8.
+
+    Returns:
+        torch.tensor: similarity of matrix. shape=(entity_count, entity_count)
+    """
+    a_n, b_n = a.norm(dim=1)[:, None], b.norm(dim=1)[:, None]
+    a_norm = a / torch.max(a_n, eps * torch.ones_like(a_n))
+    b_norm = b / torch.max(b_n, eps * torch.ones_like(b_n))
+    sim_mt = torch.mm(a_norm, b_norm.transpose(0, 1))
+    return sim_mt
+
+
+def get_entity_list(
+    sents: List[Sentence], embeddings: StackedEmbeddings, tag_type: str = "ner"
+) -> List:
+    """Get all entities from sentences
+
+    Args:
+        sents (List[Sentence]): Flair sentences
+        tag_type (str): Label type, e.g. "ner"
+        embeddings: The embeddings method
+
+    Returns:
+        List: Get all entities. Each entity has {sent_idx, token_idx, token_text, token_embedding}
+    """
+    label_entity_list = []
+    for sent_idx, sent in enumerate(sents):
+        if len(sent.get_spans(tag_type)) != 0:
+            embeddings.embed(sent)
+            for token_idx, token in enumerate(sent):
+                tag = token.get_tag(tag_type)
+                if (
+                    tag.value == "O"
+                ):  # Skip if the "O" label. tag.value is the label name
+                    continue
+                tag_info = {
+                    "sent_idx": sent_idx,
+                    "token_idx": token_idx,
+                    "token_text": token.text,
+                    "token_embedding": token.embedding,
+                }
+                label_entity_list.append(tag_info)
+    return label_entity_list
 
 
 def random_sampling(
@@ -71,7 +124,9 @@ def mnlp_sampling(sents: List[Sentence], tag_type: str, **kwargs) -> List[int]:
     return indices.tolist()
 
 
-def similarity_sampling(sents: List[Sentence], tag_type: str, **kwargs) -> List[int]:
+def similarity_sampling(
+    sents: List[Sentence], tag_type: str = "ner", **kwargs
+) -> List[int]:
     """Similarity sampling
 
     We create similarity sampling as a kind of diversity sampling method.
@@ -80,11 +135,11 @@ def similarity_sampling(sents: List[Sentence], tag_type: str, **kwargs) -> List[
     We calculate the similarity between entity pair, the low similarity pair means high diversity.
 
     Args:
-        sents (List[Sentence]): flair sentences
-        tag_type (str): label type, e.g. "ner"
+        sents (List[Sentence]): Flair sentences
+        tag_type (str): Label type, e.g. "ner"
     kwargs:
-        label_names (List[str]): label name of all dataset
-        embeddings: the embeddings method
+        label_names (List[str]): Label name of all dataset
+        embeddings: The embeddings method
 
     Returns:
         List[int]:
@@ -94,7 +149,6 @@ def similarity_sampling(sents: List[Sentence], tag_type: str, **kwargs) -> List[
     if "O" in label_names:
         label_names.remove("O")
     embeddings = kwargs["embeddings"]
-    embedding_dim = None
 
     # Get entities in each class, each entity has {sent_idx, token_idx, token_text, token_embedding}
     label_entity_list = {label: [] for label in label_names}
@@ -112,22 +166,23 @@ def similarity_sampling(sents: List[Sentence], tag_type: str, **kwargs) -> List[
                         "token_text": token.text,
                         "token_embedding": token.embedding,
                     }
-                    if embedding_dim is None:
-                        embedding_dim = len(token.embedding.shape) - 1
                     label_entity_list[tag.value].append(tag_info)
 
-    # Calculate similarity of entity pair
-    # (entity1, entity2, similarity)
-    # {"ORG": [({sent_idx, token_idx, token_text, token_embedding}, {sent_idx, ...}, 0.98), (), ... ]}
+    # Assign similarity score to entity pair
     label_entity_pair_similarity = {label: [] for label in label_names}
-    cos = nn.CosineSimilarity(dim=embedding_dim)
     for label, entity_list in label_entity_list.items():
+        class_entity_embedding_matrix = [tag["token_embedding"] for tag in entity_list]
+        if class_entity_embedding_matrix == []:
+            continue
+        # Calculate similarity of entity pair
+        class_entity_embedding_matrix = torch.stack(class_entity_embedding_matrix)
+        class_entity_embedding_sim_matrix = sim_matrix(
+            class_entity_embedding_matrix, class_entity_embedding_matrix
+        )
         length = len(entity_list)
         for i in range(length - 1):
             for j in range(i + 1, length):
-                cosine_score = cos(
-                    entity_list[i]["token_embedding"], entity_list[j]["token_embedding"]
-                )
+                cosine_score = class_entity_embedding_sim_matrix[i][j]
                 triple = (entity_list[i], entity_list[j], cosine_score)
                 label_entity_pair_similarity[label].append(triple)
 
@@ -144,17 +199,19 @@ def similarity_sampling(sents: List[Sentence], tag_type: str, **kwargs) -> List[
             cosine_score = entity_pair[2]
 
             entity1 = entity_pair[0]
-            sentence_score[entity1["sent_idx"]] += cosine_score
+            sentence_score[entity1["sent_idx"]] -= cosine_score
 
             entity2 = entity_pair[1]
-            sentence_score[entity2["sent_idx"]] += cosine_score
+            sentence_score[entity2["sent_idx"]] -= cosine_score
 
     ascend_indices = np.argsort(sentence_score)
 
     return ascend_indices.tolist()
 
 
-def cluster_sampling(sents: List[Sentence], tag_type: str, **kwargs) -> List[int]:
+def cluster_sampling(
+    sents: List[Sentence], tag_type: str = "ner", **kwargs
+) -> List[int]:
     """Cluster sampling.
 
     We create cluster sampling as a kind of diversity sampling method.
@@ -165,11 +222,12 @@ def cluster_sampling(sents: List[Sentence], tag_type: str, **kwargs) -> List[int
     the low similarity pair means high diversity.
 
     Args:
-        sents (List[Sentence]): [description]
-        tag_type (str): [description]
+        sents (List[Sentence]): Flair sentences
+        tag_type (str): Label type, e.g. "ner"
 
     Returns:
-        List[int]: [description]
+        List[int]:
+            query_idx: The index of queried samples in sents.
     """
     label_names = kwargs["label_names"]
     if "O" in label_names:
@@ -178,95 +236,65 @@ def cluster_sampling(sents: List[Sentence], tag_type: str, **kwargs) -> List[int
     embedding_dim = None
 
     # Get entities in each class, each entity has {sent_idx, token_idx, token_text, token_embedding}
-    label_entity_list = []
-
-    for sent_idx, sent in enumerate(sents):
-        if len(sent.get_spans("ner")) != 0:
-            embeddings.embed(sent)
-            for token_idx, token in enumerate(sent):
-                tag = token.get_tag("ner")
-                if (
-                    tag.value == "O"
-                ):  # Skip if the "O" label. tag.value is the label name
-                    continue
-                tag_info = {
-                    "sent_idx": sent_idx,
-                    "token_idx": token_idx,
-                    "token_text": token.text,
-                    "token_embedding": token.embedding,
-                }
-                if embedding_dim is None:
-                    embedding_dim = len(token.embedding.shape) - 1
-                label_entity_list.append(tag_info)
+    label_entity_list = get_entity_list(sents, embeddings, tag_type=tag_type)
 
     # Get all entity embedding matrix
     entity_embedding_matrix = [tag["token_embedding"] for tag in label_entity_list]
     if entity_embedding_matrix == []:
         return random_sampling(sents)
-    else:
-        entity_embedding_matrix = stack(entity_embedding_matrix)
+    entity_embedding_matrix = torch.stack(
+        entity_embedding_matrix
+    )  # e.g. shape=(36, 100)
+
+    if embedding_dim is None:  # for nn.consine similarity
+        embedding_dim = len(entity_embedding_matrix[0].shape) - 1
 
     # Clustering
     kmeans = KMeans(n_clusters=len(label_names))
     kmeans.fit(entity_embedding_matrix)
 
-    cluster_centers_matrix = kmeans.cluster_centers_
-    entity_labels = kmeans.labels_
-
-    # Find the center in matrix
-    center_cluster_num = {}  # {center_num_in_cluster: center_index_in_matrix}
-    for i, token_matrix in enumerate(entity_embedding_matrix):
-        for center_matrix in cluster_centers_matrix:
-            if center_matrix == token_matrix:
-                center_num_in_cluster = entity_labels[i]
-                center_cluster_num[center_num_in_cluster] = i
+    # If the algorithm stops before fully converging (see tol and max_iter), these will not be consistent with labels_.
+    # Which means the cluster_centers_ is not the real entity
+    cluster_centers_matrix = kmeans.cluster_centers_  # e.g. shape is (4, 100)
+    entity_labels = (
+        kmeans.labels_
+    )  # e.g. [0, 2, 3, 1, ...], the number is the indices of index in cluster_centers_matrix
 
     # Find the entity in each cluster
+    center_cluster_index = list(range(cluster_centers_matrix.shape[0]))
     label_entity_cluster = {
-        cluster_num: {"cluster_center_idx": 0, "cluster_member_idx": []}
-        for cluster_num in center_cluster_num.keys()
+        cluster_num: {"cluster_member_idx": []} for cluster_num in center_cluster_index
     }
     for cluster_num in label_entity_cluster.keys():
-        label_entity_cluster[cluster_num]["cluster_center"] = center_cluster_num[
-            cluster_num
-        ]
         for i, entity_cluster_num in enumerate(entity_labels):
             if entity_cluster_num == cluster_num:
                 label_entity_cluster[cluster_num]["cluster_member_idx"].append(i)
 
-    # Calculate each the similarity between center and entities
-    for cluster_num, cluster_info in label_entity_cluster.items():
-        center_idx = cluster_info["cluster_center_idx"]
-        scores = []
-        for member_idx in cluster_info["cluster_member_idx"]:
-            cos = nn.CosineSimilarity(dim=embedding_dim)
-            cosine_score = cos(
-                entity_embedding_matrix[center_idx], entity_embedding_matrix[member_idx]
-            )
-            scores.append(cosine_score)
-        label_entity_cluster["sim_scores"] = scores
-
-    # Used for debug the order
-    for cluster_num, cluster_info in label_entity_cluster.items():
-        cluster_member_idx = cluster_info["cluster_member_idx"]
-        sim_scores = cluster_info["sim_scores"]
-
-        cluster_info["sim_scores"] = [
-            x for _, x in sorted(zip(sim_scores, cluster_member_idx))
-        ]
-        cluster_info["cluster_member_idx"] = sorted(sim_scores)
+    # Calculate each the similarity between center and entity
+    for cluster_num, cluster_dict in label_entity_cluster.items():
+        center_matrix = cluster_centers_matrix[cluster_num]  # shape (100,)
+        center_matrix = center_matrix.reshape(1, len(center_matrix))  # shape (1, 100)
+        member_matrix = entity_embedding_matrix[
+            cluster_dict["cluster_member_idx"]
+        ]  # shape (8, 100)
+        scores = (
+            sim_matrix(center_matrix, member_matrix).reshape(-1).tolist()
+        )  # shape(8)
+        label_entity_cluster[cluster_num]["sim_scores"] = scores
 
     # Flat the entity score
     entity_scores = [0] * len(label_entity_list)
-    for cluster_num, cluster_info in label_entity_cluster.items():
-        for i, member_idx in enumerate(cluster_info["cluster_member_idx"]):
-            entity_scores[member_idx] += cluster_info["sim_scores"][i]
+    for cluster_num, cluster_dict in label_entity_cluster.items():
+        for i, member_idx in enumerate(cluster_dict["cluster_member_idx"]):
+            entity_scores[member_idx] += cluster_dict["sim_scores"][i]
 
     # Reorder the sentence index
-    sentence_scores = [99] * len(sents)
+    sentence_scores = [0] * len(sents)
     for entity_idx, entity_info in enumerate(label_entity_list):
         sent_idx = entity_info["sent_idx"]
-        sentence_scores[sent_idx] += entity_scores[entity_idx]
+        # The entity_scores is smaller, means similarity is low, the diversity is high
+        # After minus, the sentence_scores is bigger
+        sentence_scores[sent_idx] -= entity_scores[entity_idx]
 
     ascend_indices = np.argsort(sentence_scores)
 
