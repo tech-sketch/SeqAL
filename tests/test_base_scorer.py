07da755acc3bb2cfc110ae7ce1378f28f8074440
BrambleXu <liangxu006@gmail.com>
2022-01-28 17:10:39 +0900

    test: add new scorer test template

---
+++ tests/test_base_scorer.py
@@ -0,0 +1,29 @@
+class TestBaseScorer:
+    def query(self):
+        # If query_number is bigger than number of scores
+
+        # If query_number is smaller than number of scores
+
+        pass
+
+    def test_similarity_matrix(self):
+        # If input is array, change it to tensor
+
+        # Normalize input dtype is to tensor.float32
+
+        # Raise error if input shape is not compatible
+
+        # Empty matrix
+
+        # Expected output
+
+        pass
+
+    def test_normalize_score(self):
+        # If input is list, change it to array
+
+        # Check input type array
+
+        # Expected output
+
+        pass
