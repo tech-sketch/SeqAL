6aa34005b899bcb1a0139f02b88119c922cbb9a9
BrambleXu <liangxu006@gmail.com>
2022-03-09 08:53:57 +0900

    feat: add load_plain_text function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,15 +1,16 @@
+from pathlib import Path
 from typing import List
 from unittest.mock import MagicMock
 
 from flair.data import Sentence
 
+from seqal import utils
 from seqal.datasets import Corpus
-from seqal.utils import add_tags, assign_id_corpus, entity_ratio
 
 
 def test_assign_id_corpus(corpus: Corpus) -> None:
     """Test assign_id_corpus function"""
-    corpus_with_ids = assign_id_corpus(corpus)
+    corpus_with_ids = utils.assign_id_corpus(corpus)
     train = corpus_with_ids.train
     dev = corpus_with_ids.dev
     test = corpus_with_ids.test
@@ -29,7 +30,7 @@ def test_add_tags() -> None:
         {"text": "This book is great.", "labels": []},
     ]
 
-    annotated_sents = add_tags(query_labels)
+    annotated_sents = utils.add_tags(query_labels)
     assert len(annotated_sents[0].get_spans("ner")) == 1
     assert len(annotated_sents[1].get_spans("ner")) == 0
 
@@ -37,7 +38,7 @@ def test_add_tags() -> None:
 def test_entity_ratio_return_0(unlabeled_sentences: List[Sentence]):
     """Test entity_ratio if no entities"""
     # Act
-    result = entity_ratio(unlabeled_sentences)
+    result = utils.entity_ratio(unlabeled_sentences)
 
     # Assert
     assert result == 0
@@ -58,7 +59,20 @@ def test_entity_ratio():
     sentences = [sentence1, sentence2]
 
     # Act
-    result = entity_ratio(sentences)
+    result = utils.entity_ratio(sentences)
 
     # Assert
     assert result == float(2)
+
+
+def test_load_plain_text(fixture_path: Path) -> None:
+    """Test load_plain_text function"""
+    # Arrange
+    sentence1 = "Germany imported 47,600 sheep from Britain last year , nearly half of total imports ."
+    file_path = fixture_path / "conll/plain_dataset.txt"
+
+    # Act
+    sentences = utils.load_plain_text(file_path)
+
+    # Assert
+    assert sentences[1].to_plain_string() == sentence1
