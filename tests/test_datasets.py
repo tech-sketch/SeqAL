be65823bdd5a363c480677bdd2480c3985c020b8
BrambleXu <liangxu006@gmail.com>
2022-03-11 13:45:09 +0900

    test: add test_add_queried_samples for Corpus

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -2,8 +2,22 @@ from seqal.datasets import Corpus
 
 
 class TestCorpus:
+    """Test Corpus class"""
+
     def test_get_all_sentences(self, corpus: Corpus) -> None:
+        """Test get_all_sentences function"""
         assert len(corpus.get_all_sentences()) == 20
+
+    def test_add_queried_samples(self, corpus: Corpus) -> None:
+        """Test add_queried_samples function"""
+        # Arrange
+        queried_samples = corpus.dev.sentences
+
+        # Act
+        corpus.add_queried_samples(queried_samples)
+
+        # Assert
+        assert len(corpus.train.sentences) == 15
 
 
 class TestColumnCorpus:
